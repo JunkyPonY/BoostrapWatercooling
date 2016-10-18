a5c04cd3ac3947c8df8b2870ff1a74f7f55669e0
Elouan Raffray <Elouan Raffray>
2016-10-18 18:59:48 +0200

    the end ?

--- js/index.js
+++ js/index.js
@@ -28,3 +28,46 @@ function moveScroller() {
     $(window).scroll(move);
     move();
 }
+
+function slide() {
+    var p=1;
+    var slide1;
+    var slide2;
+    document.getElementById("slide1").style["left"] = "0";
+    document.getElementById("slide2").style["left"] = "100vw";
+    document.getElementById("slide3").style["left"] = "200vw";
+
+    $("#slidenext").click(function() {
+
+        if ( p < 3) {
+            $("#slide1").animate({ left: "-=100vw" });
+            $("#slide2").animate({ left: "-=100vw" });
+            $("#slide3").animate({ left: "-=100vw" });
+            p++;
+
+            if (p == 3) {
+                $("#slidenext").css("background-color", "#10131a");
+            }
+            $("#slideprev").css("background-color", "#00D054");
+        }
+
+
+    });
+
+
+    $("#slideprev").click(function() {
+        
+        if (p > 1) {
+            $("#slide1").animate({ left: "+=100vw" });
+            $("#slide2").animate({ left: "+=100vw" });
+            $("#slide3").animate({ left: "+=100vw" });
+            p--;
+
+            if (p == 1) {
+                $("#slideprev").css("background-color", "#10131a");
+            }
+            $("#slidenext").css("background-color", "#00D054");
+        }
+
+    });
+}
